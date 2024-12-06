8ebe9fc19ffd0c17f7e5a390c01c2980ff3a0fcd
Siddharth Saran <siddharth.saran@gmail.com>
2024-12-06 15:43:29 -0500

    Changes upto themeing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="text-lg">About</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,58 @@
+import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
+
+const TICKET_ICONS = {
+  OPEN: <LucideFileText />,
+  DONE: <LucideCheckCircle />,
+  IN_PROGRESS: <LucidePencil />,
+};
+
+const TicketsPage = () => {
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
+
+      <Separator />
+
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        {initialTickets.map((ticket) => (
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2">
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <span className="truncate">{ticket.title}</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <span className="line-clamp-3 whitespace-break-spaces">
+                {ticket.content}
+              </span>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicketsPage;
