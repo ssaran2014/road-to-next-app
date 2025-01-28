ef8b40d1e8c0ed7762a3316cc0d7ee89522ef0cf
Siddharth Saran <siddharth.saran@gmail.com>
2025-01-28 13:04:13 -0500

    use-debounce

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,33 +1,37 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 import { Input } from "./ui/input";
 
 type SearchInputProps = {
-    placeholder: string;
+  placeholder: string;
 };
 
-const SearchInput = ({placeholder}: SearchInputProps) => {
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
+const SearchInput = ({ placeholder }: SearchInputProps) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        const params = new URLSearchParams(searchParams);
+  const handleSearch = useDebouncedCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      const params = new URLSearchParams(searchParams);
 
-        if (value) {
-            params.set("search", value);
-        } else {
-            params.delete("search");
-        }
+      if (value) {
+        params.set("search", value);
+      } else {
+        params.delete("search");
+      }
 
-        replace(`${pathname}?${params.toString()}`, {
-            scroll: false,
-        })
-    };
-    
-    return <Input placeholder={placeholder} onChange={handleSearch} />;
+      replace(`${pathname}?${params.toString()}`, {
+        scroll: false,
+      });
+    },
+    250
+  );
+
+  return <Input placeholder={placeholder} onChange={handleSearch} />;
 };
 
 export { SearchInput };
